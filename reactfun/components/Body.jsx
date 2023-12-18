4ff75490535d449e2e0f39d5c9366df1b004489a
unknown <Breke64@gmail.com>
2023-12-18 08:20:43 -0500

    gonna find a idea for starting project

--- reactfun/components/Body.jsx
+++ reactfun/components/Body.jsx
@@ -4,9 +4,18 @@ export default function Body() {
 
 
   return (
-
+    
     <div>
       <p>Body Here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
+      <p>Body also here</p>
     </div>
   )
 }
